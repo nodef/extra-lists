2180303a91bcfd1cf793ccd5b87e09b13a0a7e93
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 02:24:50 +0530

    :watermelon: cool fruit +some

--- src/some.js
+++
@@ -1,7 +0,0 @@
-function some(lst, fn, ths) {
-  var vi = lst[1][Symbol.iterator]();
-  for(var k of lst[0])
-    if(fn.call(ths, vi.next().value, k, lst)) return true;
-  return false;
-};
-module.exports = some;
---
+++ src/some.ts
@@ -0,0 +1,28 @@
+import type {Lists, testFn} from './_types';
+
+function someIf<T, U>(x: Lists<T, U>): boolean {
+  for(var v of x[1])
+    if(v) return true;
+  return false;
+}
+
+function someTest<T, U>(x: Lists<T, U>, fn: testFn<T, U>, ths: object=null): boolean {
+  var [ks, vs] = x, ki = ks[Symbol.iterator]();
+  for(var v of vs) {
+    var k = ki.next().value;
+    if(fn.call(ths, v, k, x)) return true;
+  }
+  return false;
+}
+
+/**
+ * Checks if any value satisfies a test.
+ * @param x lists
+ * @param fn test function (v, k ,x)
+ * @param ths this argument
+ */
+function some<T, U>(x: Lists<T, U>, fn: testFn<T, U>=null, ths: object=null): boolean {
+  if(fn) return someTest(x, fn, ths);
+  else return someIf(x);
+}
+export default some;
