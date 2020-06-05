a6cd8192962c1dcb01c1af56bd528af647755118
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 16:05:08 +0530

    :watermelon: cool fruit +max will not work so sad

---
+++ src/max.ts
@@ -0,0 +1,13 @@
+import {max as iterableMax} from 'extra-iterable';
+import type {Lists, compareFn, mapFn} from './_types';
+
+/**
+ * Finds largest value.
+ * @param x an iterable
+ * @param fc compare function (a, b)
+ * @param fm map function (v, i, x)
+ */
+function max<T, U, V=U>(x: Lists<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): U {
+  return iterableMax(x[1], fc, fm);
+}
+// (will not work so sad)
