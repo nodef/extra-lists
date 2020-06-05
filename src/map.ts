1bee3582a26623bde82642f574d155da52898be9
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 15:43:55 +0530

    :watermelon: cool fruit build fails

--- src/map.ts
+++ src/map.ts
@@ -6,21 +6,7 @@ import type {Lists, mapFn} from './_types';
  * @param fn map function (v, k, x)
  * @param ths this argument
  */
-function map<T, U, V>(x: Lists<T, U>, fn: mapFn<T, U, V>, ths: object=null): Lists<T, V> {
+function* map<T, U, V>(x: Lists<T, U>, fn: mapFn<T, U, V>, ths: object=null) {
   var i = -1;
-  for(var v of x)
-    yield fn.call(ths, v, ++i, x);
 }
 export default map;
-
-
-function map(lst, fn, ths) {
-  var i = 0, y = [], z = [];
-  var vi = lst[1][Symbol.iterator]();
-  for(var k of lst[0]) {
-    y[i] = k;
-    z[i++] = fn.call(ths, vi.next().value, k, lst);
-  }
-  return [y, z];
-};
-module.exports = map;
