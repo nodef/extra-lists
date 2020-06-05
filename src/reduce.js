a32e0dffac09389d3195664f3c9f7a16e387d10f
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 15:38:43 +0530

    :watermelon: cool fruit +reduce

--- src/reduce.js
+++
@@ -1,9 +0,0 @@
-function reduce(lst, fn, acc) {
-  var vi = lst[1][Symbol.iterator]();
-  for(var k of lst[0]) {
-    var v = vi.next().value;
-    acc = acc!==undefined? fn(acc, v, k, lst):v;
-  }
-  return acc;
-};
-module.exports = reduce;
---
+++ src/reduce.ts
@@ -0,0 +1,19 @@
+import type {Lists, reduceFn} from './_types';
+
+/**
+ * Reduces values to a single value.
+ * @param x lists
+ * @param fn reduce function (acc, v, k, x)
+ * @param acc initial value
+ */
+function reduce<T, U, V>(x: Lists<T, U>, fn: reduceFn<T, U, V>, acc?: V): V {
+  var [ks, vs] = x, ki = ks[Symbol.iterator]();
+  var init = arguments.length <= 2;
+  for(var v of vs) {
+    var k = ki.next().value;
+    if(init) { init = false; acc = v as any as V; }
+    else acc = fn(acc, v, k, x);
+  }
+  return acc;
+}
+export default reduce;
