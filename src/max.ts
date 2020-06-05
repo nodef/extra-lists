1d0501e7b4495ec9fd2bc8d2f1f87d97f107b686
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 21:39:39 +0530

    :watermelon: cool fruit +search

--- src/max.ts
+++ src/max.ts
@@ -8,6 +8,8 @@ import type {Lists, compareFn, mapFn} from './_types';
  * @param fm map function (v, i, x)
  */
 function max<T, U, V=U>(x: Lists<T, U>, fc: compareFn<U|V>=null, fm: mapFn<T, U, U|V>=null): U {
+  // @ts-ignore
   return iterableMax(x[1], fc, fm);
+  // (will not work so sad)
 }
-// (will not work so sad)
+export default max;
