6e059cb7e4488d24d5f357972e9efcc771014751
Subhajit Sahu <wolfram77@gmail.com>
2020-06-05 18:08:21 +0530

    :watermelon: cool fruit findAll = filter

--- src/find-all.js
+++
@@ -1,9 +0,0 @@
-function findAll(lst, fn, ths) {
-  var vi = lst[1][Symbol.iterator](), i = -1, z = [];
-  for(var k of lst[0]) {
-    var v = vi.next().value;
-    if(fn.call(ths, v, k, lst)) z[++i] = v;
-  }
-  return z;
-};
-module.exports = findAll;
