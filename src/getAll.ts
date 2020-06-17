883e496c62d6613cf6740e6b7adcf308416250c5
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 23:11:02 +0530

    :watermelon: cool fruit ;getAll

--- src/getAll.ts
+++ src/getAll.ts
@@ -1,3 +1,5 @@
+import entries from './entries';
+import {getAll as mapGetAll} from 'extra-map';
 import type {Lists} from './_types';
 
 /**
@@ -5,24 +7,7 @@ import type {Lists} from './_types';
  * @param x lists
  * @param ks keys
  */
-function* getAll<T, U>(x: Lists<T, U>, ks: Iterable<T>): IterableIterator<U> {
-  var [js, vs] = x, m = new Map();
-  var ji = js[Symbol.iterator]();
-  var ki = ks[Symbol.iterator]();
-  var {value, done} = ki.next();
-  if(done) return;
-  for(var v of vs) {
-    var j = ji.next().value;
-    m.set(j, v);
-  }
-  // for(var v of x) {
-  //   while(value<=j) {
-  //     var {value, done} = ii.next();
-  //     if(done) return;
-  //     if(value<=j) yield undefined;
-  //   }
-  //   if(value===++j) yield v;
-  // }
-  // while(!ii.next().done) yield undefined;
+function getAll<T, U>(x: Lists<T, U>, ks: T[]): Iterable<U> {
+  return mapGetAll(new Map(entries(x)), ks);
 }
 export default getAll;
