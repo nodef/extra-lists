44355a0f7724752c09959ca2cd66b21743669cbe
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 23:29:35 +0530

    :watermelon: cool fruit ;join

--- src/join.ts
+++ src/join.ts
@@ -1,18 +1,14 @@
+import entries from './entries';
+import {join as mapJoin} from 'extra-map';
 import type {Lists} from './_types';
 
 /**
  * Joins values together.
  * @param x lists
  * @param sep separator (,)
- * @param asc associator
+ * @param asc associator (=)
  */
-function join<T, U>(x: Lists<T, U>, sep: string=',', asc: string=null): string {
-  var [ks, vs] = x, ki = ks[Symbol.iterator](), a = '';
-  for(var v of vs) {
-    var k = ki.next().value;
-    if(asc) a += k+asc;
-    a += v+sep;
-  }
-  return a.slice(0, -sep.length);
+function join<T, U>(x: Lists<T, U>, sep: string=',', asc: string='='): string {
+  return mapJoin(entries(x), sep, asc);
 }
 export default join;
