ef86641b7e39825d7dcfbbed3e286e6d03edcf62
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 22:12:47 +0530

    :watermelon: cool fruit ;swap

--- src/swap.ts
+++ src/swap.ts
@@ -1,13 +1,16 @@
-import swap$ from './swap$';
-import type {Entries} from './_types';
+import keys from './keys';
+import {map as iterableMap} from 'extra-iterable';
+import type {Lists} from './_types';
+import values from './values';
 
 /**
  * Exchanges two values.
- * @param x a map
+ * @param x lists
  * @param k a key
  * @param l another key
  */
-function swap<T, U>(x: Entries<T, U>, k: T, l: T): Map<T, U> {
-  return swap$(new Map(x), k, l);
+function swap<T, U>(x: Lists<T, U>, k: T, l: T): Lists<T, U> {
+  var ks = iterableMap(keys(x), j => j===k? l : (j===l? k : j));
+  return [ks, values(x)];
 }
 export default swap;
