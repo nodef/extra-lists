6102c4f6bcb17d90127c5cde8c3905d6a31e062b
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 22:00:46 +0530

    :watermelon: cool fruit ;some

--- src/some.ts
+++ src/some.ts
@@ -1,27 +1,13 @@
-import type {Lists, testFn} from './_types';
-
-function someIf<T, U>(x: Lists<T, U>): boolean {
-  for(var v of x[1])
-    if(v) return true;
-  return false;
-}
-
-function someTest<T, U>(x: Lists<T, U>, fn: testFn<T, U>, ths: object=null): boolean {
-  var [ks, vs] = x, ki = ks[Symbol.iterator]();
-  for(var v of vs) {
-    var k = ki.next().value;
-    if(fn.call(ths, v, k, x)) return true;
-  }
-  return false;
-}
+import entries from './entries';
+import {some as mapSome} from 'extra-map';
+import type {testFn, Lists} from './_types';
 
 /**
  * Checks if any value satisfies a test.
  * @param x lists
- * @param fn test function (v, k ,x)
+ * @param ft test function (v, k, x)
  */
-function some<T, U>(x: Lists<T, U>, fn: testFn<T, U>=null): boolean {
-  if(fn) return someTest(x, fn, ths);
-  else return someIf(x);
+function some<T, U>(x: Lists<T, U>, ft: testFn<T, U>=null): boolean {
+  return mapSome(entries(x), ft as any);
 }
 export default some;
