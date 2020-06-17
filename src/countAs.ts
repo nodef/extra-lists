967810f99b67e29c633014380ea0e9820c47e336
Subhajit Sahu <wolfram77@gmail.com>
2020-06-17 21:51:28 +0530

    :watermelon: cool fruit ;countAs

--- src/countAs.ts
+++ src/countAs.ts
@@ -1,22 +1,14 @@
-import id from './_id';
-import keys from './keys';
-import values from './values';
+import entries from './entries';
+import {countAs as mapCountAs} from 'extra-map';
 import type {Lists, mapFn} from './_types';
 
 /**
  * Counts occurrences of values.
  * @param x lists
- * @param fn map function (v, k, x)
+ * @param fm map function (v, k, x)
  * @returns Map {value => count}
  */
-function countAs<T, U, V=U>(x: Lists<T, U>, fn: mapFn<T, U, U|V>=null): Map<U|V, number> {
-  var fn = fn||id, a = new Map();
-  var vi = values(x)[Symbol.iterator]();
-  for(var k of keys(x)) {
-    var v = vi.next().value;
-    var v1 = fn(v, k, x);
-    a.set(v1, (a.get(v1)||0) + 1);
-  }
-  return a;
+function countAs<T, U, V=U>(x: Lists<T, U>, fm: mapFn<T, U, U|V>=null): Map<U|V, number> {
+  return mapCountAs(entries(x), fm as any);
 }
 export default countAs;
