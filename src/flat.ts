cbb602debc476bc85a730fcc17156b2e3c2ffcee
Subhajit Sahu <wolfram77@gmail.com>
2020-06-18 10:37:35 +0530

    :watermelon: cool fruit ;flatMap

--- src/flat.ts
+++ src/flat.ts
@@ -1,20 +1,22 @@
 import is from './is';
-import type {Entries} from './_types';
+import entries from './entries';
+import type {testFn, Lists} from './_types';
 
-function flatTo<T>(x: Entries<T, any>, dep: number, a: Map<T, any>): Map<T, any> {
-  for(var [k, v] of x) {
-    if(dep!==0 && is(v)) flatTo(v, dep-1, a);
+function flatTo<T>(x: Lists<T, any>, n: number, ft: testFn<T, any>, a: Map<T, any>): Map<T, any> {
+  for(var [k, v] of entries(x)) {
+    if(n!==0 && ft(v, k, x)) flatTo(v, n-1, ft, a);
     else a.set(k, v);
   }
   return a;
 }
 
 /**
- * Flattens nested map to given depth.
- * @param x a nested map
- * @param dep maximum depth (-1)
+ * Flattens nested lists to given depth.
+ * @param x nested lists
+ * @param n maximum depth (-1)
+ * @param ft test function (v, k, x)
  */
-function flat<T>(x: Entries<T, any>, dep: number=-1): Map<T, any> {
-  return flatTo(x, dep, new Map());
+function flat<T>(x: Lists<T, any>, n: number=-1, ft: testFn<T, any>=null): Map<T, any> {
+  return flatTo(x, n, ft||is, new Map());
 }
 export default flat;
--- src/flatMap.ts
+++ src/flatMap.ts
@@ -1,21 +1,25 @@
 import id from './_id';
 import is from './is';
-import concat$ from './concat$';
-import type {mapFn, Entries} from './_types';
+import entries from './entries';
+import {concat$} from 'extra-map'
+import type {mapFn, testFn, Lists} from './_types';
 
 /**
- * Flattens nested map, using map function.
- * @param x a nested map
- * @param fn map function (v, k, x)
+ * Flattens nested lists, using map function.
+ * @param x nested lists
+ * @param fm map function (v, k, x)
+ * @param ft test function (v, k, x)
  */
-function flatMap<T>(x: Entries<T, any>, fn: mapFn<T, any, any>=null): Map<T, any> {
-  var fn = fn||id;
+function flatMap<T>(x: Lists<T, any>, fm: mapFn<T, any, any>=null, ft: testFn<T, any>=null): Lists<T, any> {
+  var fm = fm||id, ft = ft||is;
   var a = new Map();
-  for(var [k, v] of x) {
-    var v1 = fn.call(ths, v, k, x);
-    if(is(v1)) concat$(a, v1);
+  for(var [k, v] of entries(x)) {
+    var v1 = fm(v, k, x);
+    if(ft(v1, k, x)) concat$(a, entries(v1));
     else a.set(k, v1);
   }
-  return a;
+  return [a.keys(), a.values()];
 }
 export default flatMap;
+// REUSE mapFlatMap?
+// YE
