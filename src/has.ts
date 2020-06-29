e05c5e8b668db4b0f6d93f9dc61bbff99345b2c4
Subhajit Sahu <wolfram77@gmail.com>
2020-06-29 19:12:51 +0530

    :coconut: chutney codegen

--- src/has.ts
+++ src/has.ts
@@ -1,5 +1,5 @@
 import keys from './keys';
-import {isValue as iterableIsValue} from 'extra-iterable';
+import {hasValue as iterableHasValue} from 'extra-iterable';
 import type {Lists} from './_types';
 
 /**
@@ -8,6 +8,6 @@ import type {Lists} from './_types';
  * @param k key?
  */
 function has<T, U>(x: Lists<T, U>, k: T): boolean {
-  return iterableIsValue(keys(x), k);
+  return iterableHasValue(keys(x), k);
 }
 export default has;
